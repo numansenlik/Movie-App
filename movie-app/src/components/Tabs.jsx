9485a4d2fcf92b5614eec5e940e9809ca6838686
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-13 23:48:37 +0300

    update tabs

--- movie-app/src/components/Tabs.jsx
+++ movie-app/src/components/Tabs.jsx
@@ -20,6 +20,7 @@ const Tabs = () => {
     <div className="p-5 m-5 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7">
       {tabs.map((tab, i) => (
         <Link
+          key={i}
           className={`cursor-pointer hover:opacity-80 ${
             tab.url === genre
               ? "underline underline-offset-8 text-blue-400 dark:text-orange-400"
