148e505b7d5a5b1e844d5312e7640efeebed7c37
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-13 23:47:26 +0300

    created tabs and  theme components

---
+++ movie-app/src/components/Tabs.jsx
@@ -0,0 +1,36 @@
+"use client";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+const Tabs = () => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  const tabs = [
+    {
+      name: "Populer",
+      url: "populer",
+    },
+    { name: "Latest", url: "latest" },
+    {
+      name: "Up Coming",
+      url: "upcoming",
+    },
+  ];
+  return (
+    <div className="p-5 m-5 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7">
+      {tabs.map((tab, i) => (
+        <Link
+          className={`cursor-pointer hover:opacity-80 ${
+            tab.url === genre
+              ? "underline underline-offset-8 text-blue-400 dark:text-orange-400"
+              : ""
+          }`}
+          href={`/?genre=${tab.url}`}
+        >
+          {tab.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+export default Tabs;
--- movie-app/src/components/ThemeComp.jsx
+++ movie-app/src/components/ThemeComp.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import { CiDark, CiLight } from "react-icons/ci";
 
 const ThemeComp = () => {
   const [mounted, setMounted] = useState(false);
@@ -11,6 +12,23 @@ const ThemeComp = () => {
   }, []);
 
   const themeMode = theme == "system" ? systemTheme : theme;
-  return <div>{mounted}</div>;
+  return (
+    <div>
+      {mounted &&
+        (themeMode === "dark" ? (
+          <CiDark
+            onClick={() => setTheme("light")}
+            size={25}
+            className="cursor-pointer"
+          />
+        ) : (
+          <CiLight
+            onClick={() => setTheme("dark")}
+            size={25}
+            className="cursor-pointer"
+          />
+        ))}
+    </div>
+  );
 };
 export default ThemeComp;
