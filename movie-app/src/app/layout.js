df327e528d374dd2d4d1f7da2d07c6c09eaaf400
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-15 23:41:22 +0300

    final

--- movie-app/src/app/layout.js
+++ movie-app/src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./Providers";
-import Tabs from "@/components/Tabs";
 
 const inter = Inter({ subsets: ["latin"] });
 
