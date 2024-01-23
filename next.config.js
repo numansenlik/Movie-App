40a1ffad6c906d5ea01816611f12e85a331bb6a0
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-23 17:03:13 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-      },
-    ],
-    domains: ["https:/image.tmdb.org"],
+    domains: ["image.tmdb.org"],
   },
 };
